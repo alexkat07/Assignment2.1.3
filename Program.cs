a7b39d35a6ba27cce711a2333a86816a66b8d2ed
alexk <alexk@DESKTOP-QL7PNT4>
2024-05-13 16:22:05 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Assignment2._1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    public class SimpleMath
+    {
+        static public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        static public decimal Add(decimal num1, decimal num2, decimal num3) 
+        {
+            return num1 + num2 + num3;
+        }
+        
+        static public float Multiply(float num1, float num2)
+        {
+            return num1 * num2;
+        }
+
+        static public float Multiply(float num1, float num2, float num3) 
+        {
+            return num1 * num2 * num3;
+        
+        }
+
+
+    }
+
+}
+
